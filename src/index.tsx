c71b6c9677bbdca3af42ec2676b37b61ae5e3d36
Akshay <akshay.ap95@gmail.com>
2020-06-25 21:46:35 +0530

    Add web3 check

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Web3 from 'web3';
+
+let web3 : Web3
+
+declare global {
+  interface Window {
+      web3:any;
+      ethereum:any;
+  }
+}
+
+if (window.web3) {
+  web3 = new Web3(window.web3.currentProvider)
+  window.ethereum.enable()
+}
 
 ReactDOM.render(
   <React.StrictMode>
