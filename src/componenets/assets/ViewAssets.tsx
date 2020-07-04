66ad7ee616b783b297bdfba3317771f48f850505
Akshay <akshay.ap95@gmail.com>
2020-07-05 01:09:56 +0530

    Add serach box

--- src/componenets/assets/ViewAssets.tsx
+++ src/componenets/assets/ViewAssets.tsx
@@ -1,21 +1,49 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {MyOceanContext} from '../../OceanContext';
 import {DDO} from '@oceanprotocol/squid';
-import {Grid, Card, CardContent, Typography} from '@material-ui/core';
+import {
+    Grid,
+    Card,
+    CardContent,
+    Typography,
+    TextField,
+    Button,
+    Paper,
+    makeStyles,
+    Theme,
+    createStyles
+} from '@material-ui/core';
 import AssetDetails from './AssetDetails';
 import {useSelector, useDispatch} from 'react-redux';
 import {RootState} from '../../redux';
 import {setAssetListInfo} from '../../slices/asset-list';
 
+const useStyles = makeStyles((theme : Theme) => createStyles({
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.primary
+    },
+    testField: {
+        paddingRight: theme.spacing(2),
+        textAlign: 'center',
+        width: '300px',
+        color: theme.palette.text.primary
+    }
+}),);
+
 const ViewAssets = () => {
+    const classes = useStyles();
+
     const {instance, stakeApp, web3} = useContext(MyOceanContext)
     const {assets} = useSelector((state : RootState) => state.assetList);
     const dispatch = useDispatch();
     const {getAllStakes} = stakeApp ?. methods;
+    const [search, setSearch] = useState < string > ('xzyabc123');
 
     const getData = async () => {
         console.log('loading assets...')
-        const result = instance ?. assets.search('xzyabc123')
+        const result = instance ?. assets.search(search)
         if (result !== undefined && web3 !== null) {
             const accounts: String[] = await web3.eth.getAccounts();
 
@@ -43,13 +71,32 @@ const ViewAssets = () => {
         <div>
             <Grid container
                 spacing={3}>
-                <Card>
-                    <CardContent>
+                <Grid item>
+                    <Paper className={
+                        classes.paper
+                    }>
                         <Typography>
                             ViewAssets
                         </Typography>
-                    </CardContent>
-                </Card>
+                    </Paper>
+                </Grid>
+                <Grid item>
+                    <TextField id="outlined-basic" label="Search" variant="outlined"
+                        value={search}
+                        className={
+                            classes.testField
+                        }
+                        onChange={
+                            (e) => {
+                                setSearch(e.target.value)
+                            }
+                        }/>
+                    <Button variant="contained" color="primary"
+                        onClick={getData}>
+                        Search
+                    </Button>
+                </Grid>
+
             </Grid>
             <br/>
             <div>
